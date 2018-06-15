c78d9df97af3a3312ff159edcfc394784f20989e
vipjue <1638153693@qq.com>
2018-06-15 08:40:32 +0800

    在分支new2上修改了index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var oLi = document.getElementsByTagName("li");
+var oBox = document.getElementsByClassName('box');
 for(var i  = 0;i<oLi.length;i++){
     oLi[i].index = i;
     oLi[i].onclick=function(){
        for(var j =0;j<oLi.length;j++){
            oLi[j].className="";
        } 
-       oLi[this.index].className="active"
+       oLi[this.index].className="active";
+       for(var i  = 0;i<oBox.length;i++){
+           oBox[i].style.display="none";
+       }
+       oBox[this.index].style.display="block";
     }
 }
